75b42732446580db277c2d8b24e09d8707e1fc54
juancarlosparedesa1 <jcparedesa1@uce.edu.ec>
2023-06-08 19:43:59 -0500

    Vue.JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,35 @@
 console.log('Componenete Vue')
 console.log(Vue)
 
-const app=Vue.createApp({
-template:`
-<h1>hola mundo</h1>
-<p>Desde Vue.JS</p>`
+const app = Vue.createApp({
+    //template:`
+    //<h1>hola mundo</h1>
+    //<p>Desde Vue.JS</p>
+    //<p>{{1+1}}</p>`
+    data() {
+        return {
+            nombre:'Juan Carlos',
+            apellido:'Paredes',
+            mensaje:'Hola mundo 2 desde Vue.JS'
+
+        }
+    },
+    methods:{
+        cambiarNombre(){
+            console.log('boton cambiar nombre')
+            this.nombre='Jean Carl'
+
+        },
+        cambiarApellido(){
+            console.log('boton cambiar apellido')
+            this.apellido='Angulo'
+        }
+
+
+    }
+
+    
+
 })
 
 app.mount('#myApp')
---
+++ js/vFor.js
@@ -0,0 +1,9 @@
+console.log('Componenete Vue')
+console.log(Vue)
+
+const app = Vue.createApp({
+
+
+})
+
+app.mount('#myApp2')
